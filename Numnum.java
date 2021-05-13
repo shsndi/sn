f307464c73f950dc55380e6f973712ca03a4c0b0
shsndi <84111429+shsndi@users.noreply.github.com>
2021-05-13 16:51:38 +0900

    [시험0513]PrimeNum(vi.1)
    소수와 합성수

---
+++ Numnum.java
@@ -0,0 +1,13 @@
+public class Numnum {
+    public static void main(String[] args) {
+        
+    	
+    	for(int i = 1; i <= 50; i++)
+    		{
+    		if(i%2 == 0)
+    		{System.out.println(i+"합성수");}
+    		else
+    		{System.out.println(i+"PrimeNum");}
+    }
+    }
+}
