705977eb050f195204be7065a413976c25b20804
shsndi <84111429+shsndi@users.noreply.github.com>
2021-05-13 11:44:34 +0900

    첫번째 소스(baseline)
    토탈자바

---
+++ Total.java
@@ -0,0 +1,18 @@
+
+public class Total {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//쉼표붙이기
+		
+		for(int i=1; i<=50; i++) {
+			
+		
+		
+		System.out.print(i);}
+		
+
+	}
+
+}
