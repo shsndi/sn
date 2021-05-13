3de9cc9b5bc98c28cec17f15bcc21d81de5c4897
shsndi <84111429+shsndi@users.noreply.github.com>
2021-05-13 17:13:08 +0900

    [평가문항]문제해결시나리오
    소수(PrimeNum)와 합성수구하기

--- Numnum.java
+++ Numnum.java
@@ -1,13 +1,11 @@
 public class Numnum {
     public static void main(String[] args) {
         
-    	
-    	for(int i = 1; i <= 50; i++)
-    		{
-    		if(i%2 == 0)
-    		{System.out.println(i+"합성수");}
-    		else
-    		{System.out.println(i+"PrimeNum");}
-    }
-    }
+    	int value = 10;
+    	if(value%2==0)
+    		{System.out.println("합성수");}
+    	else 
+    	    {System.out.println("소수");}
+    } 
+    
 }
